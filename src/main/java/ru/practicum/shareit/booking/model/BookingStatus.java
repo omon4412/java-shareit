9735062fc56119b0627e6c784612598a09e664ce
Vetrov Sergey <vetrov241201@yandex.ru>
2023-10-07 19:09:07 +0300

    docs: Добавил документацию для BookingStatus

--- src/main/java/ru/practicum/shareit/booking/model/BookingStatus.java
+++ src/main/java/ru/practicum/shareit/booking/model/BookingStatus.java
@@ -1,24 +1,64 @@
 package ru.practicum.shareit.booking.model;
 
+import java.util.Arrays;
+
+/**
+ * Перечисление, представляющее статус бронирования.
+ */
 public enum BookingStatus {
+    /**
+     * Ожидание подтверждения.
+     */
     WAITING,
+
+    /**
+     * Бронирование одобрено.
+     */
     APPROVED,
+
+    /**
+     * Бронирование отклонено.
+     */
     REJECTED,
+
+    /**
+     * Все статусы.
+     */
     ALL,
+
+    /**
+     * Текущее бронирование.
+     */
     CURRENT,
+
+    /**
+     * Завершённое бронирование.
+     */
     PAST,
+
+    /**
+     * Будущее бронирование.
+     */
     FUTURE,
+
+    /**
+     * Бронирование отменено.
+     */
     CANCELED,
+
+    /**
+     * Неизвестный статус.
+     */
     UNKNOWN;
 
-    public static boolean contains(String test) {
-
-        for (BookingStatus c : BookingStatus.values()) {
-            if (c.name().equals(test)) {
-                return true;
-            }
-        }
-        return false;
+    /**
+     * Проверяет, содержит ли перечисление указанное имя статуса.
+     *
+     * @param statusName Имя статуса для проверки.
+     * @return true, если перечисление содержит статус с указанным именем, в противном случае false.
+     */
+    public static boolean contains(String statusName) {
+        return Arrays.stream(BookingStatus.values())
+                .anyMatch(c -> statusName.equalsIgnoreCase(c.name()));
     }
 }
-
